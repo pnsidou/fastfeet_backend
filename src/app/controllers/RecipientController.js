18b69ec9c24d029d561f244a8d7ae74bdc131eb7
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-19 15:48:23 -0300

    feat: add recipient model and store route

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,32 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+import { Request, Response } from 'express';
+
+class RecipientController {
+  async store(req: Request, res: Response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failure' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+}
+
+export default new RecipientController();
---
+++ src/app/models/Recipient.js
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        adress: Sequelize.STRING,
+        street: Sequelize.STRING,
+        number: Sequelize.NUMBER,
+        city: Sequelize.STRING,
+        state: Sequelize.STRING,
+        zip_code: Sequelize.NUMBER,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+export default Recipient;
