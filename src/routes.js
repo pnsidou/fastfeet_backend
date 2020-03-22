99827ecd10d6f261f2fc430618b967168e4bb63a
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-22 15:16:05 -0300

    feat: add multer and configure it to handle file uploads

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from 'express';
+import Multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -7,6 +10,7 @@ import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = new Multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -16,5 +20,9 @@ routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
 routes.get('/recipients', RecipientController.index);
 routes.delete('/recipients/:id', RecipientController.delete);
+
+routes.post('/files', upload.single('file'), (req, res) =>
+  res.json({ empty: 'route is empty' })
+);
 
 export default routes;
