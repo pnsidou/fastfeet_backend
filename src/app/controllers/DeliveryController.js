37e7abe4b6cd40f80e27a8dd6b4a69344602ddbb
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-23 13:34:17 -0300

    feat: add delivery controller

---
+++ src/app/controllers/DeliveryController.js
@@ -0,0 +1,8 @@
+import * as Yup from 'yup';
+import { Request, Response } from 'express';
+
+import Delivery from '../models/Delivery';
+
+class DeliverymanController {}
+
+export default new DeliverymanController();
