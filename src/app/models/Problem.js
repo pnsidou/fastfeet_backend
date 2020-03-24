c2d5c0e7c17ca0071beff84ccc70d1dd9a4a52cd
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-24 14:52:09 -0300

    feat: delivery problems model

---
+++ src/app/models/Problem.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Problem extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        description: Sequelize.STRING,
+        delivery_id: Sequelize.INTEGER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Recipient, {
+      foreignKey: 'delivery_id',
+      as: 'delivery',
+    });
+  }
+}
+
+export default Problem;
